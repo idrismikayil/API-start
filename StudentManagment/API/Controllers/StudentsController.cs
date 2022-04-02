b782de2f1270d070dd953c4d74fd37a0fc36658c
idrismikayil <idrisrm@code.edu.az>
2022-04-02 18:45:52 +0400

    edit

---
+++ StudentManagment/API/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Data.DAL;
+using Data.Mapper;
+using Domain.Dtos;
+using Domain.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public StudentsController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStudents()
+        {
+            var students = await _context.Students.ToListAsync();
+
+            var studentDto = _mapper.Map<List<StudentDto>>(students);
+
+            return Ok(studentDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            var students = await _context.Students.FindAsync(id);
+
+            var studentDto = _mapper.Map<StudentDto>(students);
+
+            return Ok(studentDto);
+        }
+    }
+}
---
+++ StudentManagment/ClassLibrary1/Mapper/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Student, StudentDto>().ForMember(s => s.FirstName, s => s.MapFrom(src => src.Name))
+                .ForMember(s => s.LastName, s => s.MapFrom(src => src.Surname));
+        }
+    }
+}
